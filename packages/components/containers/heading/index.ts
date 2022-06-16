dadfb96e538ff9c5c0c76d9098876b1278e8f8c0
MargeBot <321-margebot@users.noreply.gitlab.protontech.ch>
2022-06-16 15:01:18 +0000

    Merge branch 'CP-4053-rebranding-feedback-modal' into 'main'
    Rebranding feedback modal
    
    See merge request web/clients!2666

--- packages/components/containers/heading/index.ts
+++ packages/components/containers/heading/index.ts
@@ -3,4 +3,3 @@ export { default as PrivateHeader } from './PrivateHeader';
 export { default as Header } from '../../components/header/Header';
 export { default as TopNavbarListItemSettingsDropdown } from './TopNavbarListItemSettingsDropdown';
 export { default as TopNavbarListItemFeedbackButton } from './TopNavbarListItemFeedbackButton';
-export { default as FeedbackModal } from './FeedbackModal';
