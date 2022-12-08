fcefa01cec06ac424a8eebc272cc98fbc44744bb
Oleg Voloshin <oleg.voloshin@proton.ch>
2022-12-08 14:23:41 +0200

    Fix read-only link placeholder
    DRVWEB-2695

--- applications/drive/src/app/store/_views/utils/useIsActiveLinkReadOnly.ts
+++ applications/drive/src/app/store/_views/utils/useIsActiveLinkReadOnly.ts
@@ -4,6 +4,7 @@ import { ShareType } from '../..';
 import useActiveShare from '../../../hooks/drive/useActiveShare';
 import { sendErrorReport } from '../../../utils/errorHandling';
 import { DecryptedLink, useLink } from '../../_links';
+import { isIgnoredError } from '../../_utils';
 import { useShareType } from './useShareType';
 
 const isLinkReadOnly = (link: DecryptedLink, shareType: ShareType) => {
@@ -18,14 +19,15 @@ export const useIsActiveLinkReadOnly = () => {
     const link = useLink();
 
     const [isReadOnly, setIsReadOnly] = useState<boolean | undefined>(undefined);
-    const lastAc = useRef<AbortController>();
+    const ongoingRequestAc = useRef<AbortController>();
 
     useEffect(() => {
         const ac = new AbortController();
 
         // Abort ongoing request to avoid fast meaningless shareType change
-        lastAc.current?.abort();
-        lastAc.current = ac;
+        ongoingRequestAc.current?.abort();
+        ongoingRequestAc.current = ac;
+        setIsReadOnly(undefined);
 
         if (shareType) {
             link.getLink(ac.signal, shareId, linkId)
@@ -34,12 +36,22 @@ export const useIsActiveLinkReadOnly = () => {
                 })
                 .catch((e) => {
                     sendErrorReport(e);
-                    setIsReadOnly(true);
+
+                    // Ignore errors caused by .abort()
+                    if (!isIgnoredError(e)) {
+                        // Assume that a link isn't read-only,
+                        // as it's the majority of all cases.
+                        setIsReadOnly(false);
+                    }
+                })
+                .finally(() => {
+                    ongoingRequestAc.current = undefined;
                 });
-        } else {
-            setIsReadOnly(true);
+        } else if (shareType === null) {
+            // Couldn't load share info, assuming it isn't read-only
+            setIsReadOnly(false);
         }
-    }, [shareId, linkId, shareType]);
+    }, [linkId, shareType]);
 
     return {
         isLoading: isReadOnly === undefined,
