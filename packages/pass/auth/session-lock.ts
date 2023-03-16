5e916bdefa4814161891eb5a7be1dfa092fdf789
Edvin CANDON <edvin.candon@proton.ch>
2023-03-16 14:43:52 +0100

    Add session locking operations in auth utils
    [IDTEAM-210]

---
+++ packages/pass/auth/session-lock.ts
@@ -0,0 +1,50 @@
+import { api } from '../api';
+import { ResponseCodeSuccess } from '../types';
+
+/**
+ * The lock check endpoint will throw if the session
+ * is locked - if the session is unlocked we'll get the
+ * a { Code: 1000 } response
+ */
+export const isSessionLocked = async () => {
+    try {
+        const { Code } = await api({ url: 'pass/v1/user/session/lock/check', method: 'get' });
+        return Code !== ResponseCodeSuccess.ProtonResponseCode_1000;
+    } catch (e) {
+        return true;
+    }
+};
+
+export const lockSessionImmediate = async (): Promise<void> => {
+    await api({
+        url: 'pass/v1/user/session/lock/force_lock',
+        method: 'post',
+    });
+};
+
+export const lockSession = async (LockCode: string, UnlockedSecs: number): Promise<string> =>
+    (
+        await api({
+            url: 'pass/v1/user/session/lock',
+            method: 'post',
+            data: { LockCode, UnlockedSecs },
+        })
+    ).LockData!.StorageToken;
+
+export const deleteSessionLock = async (LockCode: string): Promise<string> =>
+    (
+        await api({
+            url: 'pass/v1/user/session/lock',
+            method: 'delete',
+            data: { LockCode },
+        })
+    ).LockData!.StorageToken;
+
+export const unlockSession = async (LockCode: string): Promise<string> =>
+    (
+        await api({
+            url: 'pass/v1/user/session/lock/unlock',
+            method: 'post',
+            data: { LockCode },
+        })
+    ).LockData!.StorageToken;
