11f99ba8f59c56f95063740486901d4ad468adf8
Edvin CANDON <edvin.candon@proton.ch>
2023-03-23 14:03:51 +0100

    Fix generics in AbstractField

--- applications/pass-extension/src/popup/components/Fields/AbstractField.tsx
+++ applications/pass-extension/src/popup/components/Fields/AbstractField.tsx
@@ -2,25 +2,20 @@ import type { ReactNode } from 'react';
 
 import type { FieldProps } from 'formik';
 
-import { Props as TextInputControlProps } from '../Controls/TextInputControl';
-import { Props as TextareaControlProps } from '../Controls/TextareaControl';
+import { InputControlProps } from '../Controls/InputControl';
 
-type ControlProps = TextInputControlProps | TextareaControlProps;
+export type AbstractFieldProps<T extends InputControlProps> = T & FieldProps;
 
-export type AbstractFieldProps<T extends ControlProps> = FieldProps & T;
+export const AbstractField = <T extends InputControlProps>(
+    props: AbstractFieldProps<T> & { children: (props: T) => ReactNode }
+) => {
+    const { field, form, meta, children, ...rest } = props;
 
-export const AbstractField = <T extends ControlProps>({
-    field,
-    form,
-    meta,
-    children,
-    ...rest
-}: AbstractFieldProps<T> & { children: (props: T) => ReactNode }) => {
     const { name } = field;
     const { touched, errors } = form;
     const error = touched[name] && errors[name];
     const status = error ? 'error' : 'default';
-    const inputControlProps = { ...rest, ...field, status, error } as unknown as T;
+    const inputControlProps = { ...rest, ...field, status, error } as InputControlProps;
 
     return <>{children(inputControlProps)}</>;
 };
