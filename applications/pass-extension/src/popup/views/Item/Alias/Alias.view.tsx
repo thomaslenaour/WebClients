b4020cf4489605c0fc5732a17800107cd19a4d12
A.L.WORK <alexander.lemons@proton.ch>
2023-03-29 19:33:40 +0200

    IDTEAM-981: integrate alias view

--- applications/pass-extension/src/popup/views/Item/Alias/Alias.view.tsx
+++ applications/pass-extension/src/popup/views/Item/Alias/Alias.view.tsx
@@ -3,7 +3,14 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import { c } from 'ttag';
 
-import { Icon } from '@proton/components/components';
+import {
+    Collapsible,
+    CollapsibleContent,
+    CollapsibleHeader,
+    CollapsibleHeaderIconButton,
+    Icon,
+    useNotifications,
+} from '@proton/components';
 import {
     aliasDetailsRequested,
     selectMailboxesForAlias,
@@ -11,9 +18,11 @@ import {
     selectRequestStatus,
 } from '@proton/pass/store';
 import * as requests from '@proton/pass/store/actions/requests';
+import { getFormattedDateFromTimestamp } from '@proton/pass/utils/time/format';
 
-import { TextFieldValue } from '../../../../shared/components/fields';
 import { ItemTypeViewProps } from '../../../../shared/items/types';
+import { FieldsetCluster } from '../../../components/Controls/FieldsetCluster';
+import { ValueControl } from '../../../components/Controls/ValueControl';
 import { ItemViewPanel } from '../../../components/Panel/ItemViewPanel';
 
 export const AliasView: VFC<ItemTypeViewProps<'alias'>> = ({
@@ -29,12 +38,13 @@ export const AliasView: VFC<ItemTypeViewProps<'alias'>> = ({
     handleRestoreClick,
     handleDeleteClick,
 }) => {
-    const { data: item, aliasEmail, itemId } = revision;
+    const { data: item, aliasEmail, createTime, itemId } = revision;
     const {
         metadata: { name, note },
     } = item;
 
     const dispatch = useDispatch();
+    const { createNotification } = useNotifications();
     const mailboxesForAlias = useSelector(selectMailboxesForAlias(aliasEmail!));
     const aliasDetailsLoading = useSelector(selectRequestInFlight(requests.aliasDetails(aliasEmail!)));
     const aliasDetailsRequestStatus = useSelector(selectRequestStatus(requests.aliasDetails(aliasEmail!)));
@@ -47,6 +57,22 @@ export const AliasView: VFC<ItemTypeViewProps<'alias'>> = ({
             dispatch(aliasDetailsRequested({ shareId: vault.shareId, itemId, aliasEmail: aliasEmail! }));
         }
     }, [optimistic, vault, itemId, mailboxesForAlias]);
+
+    useEffect(() => {
+        if (requestFailure) {
+            createNotification({
+                type: 'warning',
+                text: c('Warning').t`Cannot retrieve mailboxes for this alias right now`,
+            });
+        }
+    }, [requestFailure]);
+
+    const copyAliasEmail = () => {
+        if (aliasEmail) {
+            navigator.clipboard.writeText(aliasEmail);
+            createNotification({ type: 'success', text: c('Info').t`Copied to clipboard` });
+        }
+    };
 
     return (
         <ItemViewPanel
@@ -63,37 +89,51 @@ export const AliasView: VFC<ItemTypeViewProps<'alias'>> = ({
             handleRestoreClick={handleRestoreClick}
             handleDeleteClick={handleDeleteClick}
         >
-            <div className="text-semibold mb0-5"> {c('Label').t`Alias`}</div>
-            <TextFieldValue>{aliasEmail!}</TextFieldValue>
-
-            <hr className="my0-5" />
-            <div className="text-bold mb0-5">{c('Label').t`Mailboxes`}</div>
-
-            <div className="mb1">
-                {aliasDetailsLoading && <div className="extension-skeleton extension-skeleton--alias-mailbox" />}
-
-                {requestFailure && (
-                    <span className="color-warning mt0-25">
-                        <Icon name="exclamation-circle" />{' '}
-                        <small>{c('Warning').t`Cannot retrieve mailboxes for this alias right now`}</small>
-                    </span>
-                )}
-
-                {ready &&
-                    mailboxesForAlias.map(({ email }) => (
-                        <div key={email} className="text-ellipsis mb0-5">
-                            {email}
-                        </div>
-                    ))}
-            </div>
+            <FieldsetCluster mode="read" as="div">
+                <ValueControl icon="alias" label={c('Label').t`Alias address`} onClick={copyAliasEmail}>
+                    {aliasEmail}
+                </ValueControl>
+                <ValueControl icon="arrow-up-and-right-big" label={c('Label').t`Forwarded to`}>
+                    {requestFailure && <div className="extension-skeleton extension-skeleton--select" />}
+                    {ready &&
+                        mailboxesForAlias.map(({ email }) => (
+                            <div key={email} className="text-ellipsis mb0-5">
+                                {email}
+                            </div>
+                        ))}
+                </ValueControl>
+            </FieldsetCluster>
 
             {note && (
-                <>
-                    <hr className="my0-5" />
-                    <div className="text-bold mb0-5">{c('Label').t`Note`}</div>
-                    <div>{note}</div>
-                </>
+                <FieldsetCluster mode="read" as="div">
+                    <ValueControl icon="note" label={c('Label').t`Note`}>
+                        <pre>{note}</pre>
+                    </ValueControl>
+                </FieldsetCluster>
             )}
+
+            <Collapsible>
+                <CollapsibleHeader
+                    disableFullWidth
+                    suffix={
+                        <CollapsibleHeaderIconButton>
+                            <Icon name="chevron-down" className="color-weak" />
+                        </CollapsibleHeaderIconButton>
+                    }
+                >
+                    <span className="flex flex-align-items-center color-weak">
+                        <Icon className="mr-1" name="info-circle" />
+                        <span>{c('Button').t`More info`}</span>
+                    </span>
+                </CollapsibleHeader>
+                <CollapsibleContent>
+                    <FieldsetCluster mode="read" as="div">
+                        <ValueControl label={c('Label').t`Created on`}>
+                            {getFormattedDateFromTimestamp(createTime)}
+                        </ValueControl>
+                    </FieldsetCluster>
+                </CollapsibleContent>
+            </Collapsible>
         </ItemViewPanel>
     );
 };
