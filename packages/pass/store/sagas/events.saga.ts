34a670b2c2a2a96bd764bc5f6b034a117aadec45
Edvin CANDON <edvin.candon@proton.ch>
2023-04-01 12:11:41 +0200

    Close event channels on sync intent
    [IDTEAM-1041]

--- packages/pass/store/sagas/events.saga.ts
+++ packages/pass/store/sagas/events.saga.ts
@@ -3,8 +3,9 @@ import type { Task } from 'redux-saga';
 import { all, cancel, fork, take } from 'redux-saga/effects';
 
 import { api } from '@proton/pass/api';
+import { or } from '@proton/pass/utils/fp';
 
-import { boot, bootSuccess, signoutSuccess, stateLock } from '../actions';
+import { boot, bootSuccess, signoutSuccess, stateLock, syncFailure, syncIntent, syncSuccess } from '../actions';
 import type { WorkerRootSagaOptions } from '../types';
 import { shareChannels } from './events/channel.share';
 import { sharesChannel } from './events/channel.shares';
@@ -15,9 +16,9 @@ function* eventsWorker(options: WorkerRootSagaOptions): Generator {
 }
 
 export default function* watcher(options: WorkerRootSagaOptions): Generator {
-    while (yield take(bootSuccess.match)) {
+    while (yield take(or(bootSuccess.match, syncSuccess.match, syncFailure.match))) {
         const events = (yield fork(eventsWorker, options)) as Task;
-        yield take([boot.match, signoutSuccess.match, stateLock.match]);
+        yield take([boot.match, signoutSuccess.match, stateLock.match, syncIntent.match]);
         yield cancel(events);
     }
 }
