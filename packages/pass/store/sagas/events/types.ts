d83ad21d524964baf6e349132b45c5e33275f90f
Edvin CANDON <edvin.candon@proton.ch>
2023-04-02 10:53:34 +0200

    Add default event error handling in `eventChannelFactory`
    - Checks for InactiveSession or SessionLocked errors and closes channel accordingly
    - Processes optional EventChannelOptions::onError afterwards
    - Extract initiating|cancelling polling actions
    - Simplify generics in EventChannel types

--- packages/pass/store/sagas/events/types.ts
+++ packages/pass/store/sagas/events/types.ts
@@ -3,26 +3,36 @@ import type { EventChannel as ReduxSagaChannel } from 'redux-saga';
 import type { Api, ApiOptions, ChannelType, ServerEvent } from '@proton/pass/types';
 import type { EventManager } from '@proton/shared/lib/eventManager/eventManager';
 
-import type { WorkerRootSagaOptions } from '../../types';
+export type EventChannelOnEvent<T extends ChannelType, O> = (
+    event: ServerEvent<T>,
+    self: EventChannel<T>,
+    options: O
+) => Generator;
 
-type BaseConfig<T extends ChannelType, Opts> = {
+export type EventChannelOnError<T extends ChannelType, O> = (
+    error: unknown,
+    self: EventChannel<T>,
+    options: O
+) => Generator;
+
+type BaseConfig<T extends ChannelType, O> = {
     api: Api;
     eventID: string;
     interval: number;
     query?: (eventId: string) => ApiOptions<void, any, any, any>;
     mapEvent?: (managerEvent: any) => ServerEvent<T>;
     onClose?: () => void;
-    onEvent: (event: ServerEvent<T>, self: EventChannel<T>, options: Opts) => Generator;
-    onError?: (error: unknown, self: EventChannel<T>, options: Opts) => Generator;
+    onEvent: EventChannelOnEvent<T, O>;
+    onError?: EventChannelOnError<T, O>;
 };
 
-export type EventChannelOptions<T extends ChannelType = ChannelType> = BaseConfig<T, WorkerRootSagaOptions> &
+export type EventChannelOptions<T extends ChannelType, O> = BaseConfig<T, O> &
     Extract<
         { type: ChannelType.USER } | { type: ChannelType.SHARES } | { type: ChannelType.SHARE; shareId: string },
         { type: T }
     >;
 
-export type EventChannel<T extends ChannelType = any> = EventChannelOptions<T> & {
+export type EventChannel<T extends ChannelType = any, O = any> = EventChannelOptions<T, O> & {
     manager: EventManager;
     channel: ReduxSagaChannel<ServerEvent<T>>;
 };
