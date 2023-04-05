8139147923d8bc19f585a1d680ae86d09acc6103
Edvin CANDON <edvin.candon@proton.ch>
2023-04-05 08:05:53 +0200

    Improve jest transpiling in `packages/pass`
    - Rely on custom babel.config.js
    - Remove extraneous dependencies
    - Remove extraneous Jest transformers
    - Remove unnecessary Jest plugins
    
    [IDTEAM-738]

--- packages/pass/jest.config.js
+++ packages/pass/jest.config.js
@@ -3,17 +3,9 @@ module.exports = {
     moduleDirectories: ['<rootDir>/node_modules', 'node_modules'],
     testEnvironment: './jest.env.js',
     transformIgnorePatterns: [
-        'node_modules/(?!(@proton/shared|@proton/components|@protontech/mutex-browser|pmcrypto|openpgp|@openpgp/web-stream-tools|@protontech/bip39|emoji-mart)/)',
+        'node_modules/(?!(@proton/shared|@proton/components|pmcrypto|openpgp|@openpgp/web-stream-tools)/)',
     ],
-    transform: {
-        '^.+\\.(m?js|tsx?)$': '<rootDir>/jest.transform.js',
-    },
-    moduleNameMapper: {
-        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm)$': '@proton/components/__mocks__/fileMock.js',
-        '\\.(css|scss|less)$': '@proton/components/__mocks__/styleMock.js',
-        '\\.(md)$': '<rootDir>/src/__mocks__/mdMock.ts',
-        '@protontech/sieve.js': '@proton/components/__mocks__/sieve.js',
-    },
+    transform: { '^.+\\.(m?js|tsx?)$': 'babel-jest' },
     reporters: ['default', ['jest-junit', { outputName: 'test-report.xml' }]],
     testTimeout: 30000,
 };
