62e1e4b900aff6acbb6e9a347ae65946461b88a9
MargeBot <321-margebot@users.noreply.gitlab.protontech.ch>
2023-07-26 14:27:01 +0000

    Merge branch 'MAILWEB-4350-update-error-banner' into 'main'
    Add status page link when API is not reachable
    
    See merge request web/clients!6512
    
    Changelog:

--- packages/shared/lib/constants.ts
+++ packages/shared/lib/constants.ts
@@ -1105,3 +1105,7 @@ export enum RELEASE_CATEGORIES {
     STABLE = 'Stable',
     EARLY_ACCESS = 'EarlyAccess',
 }
+
+export enum PROTON_WEBSITES {
+    PROTON_STATUS_PAGE = 'https://status.proton.me',
+}
