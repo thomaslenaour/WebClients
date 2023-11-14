2717c238558c8448c9d19a9cb20271271838d792
Eduardo Conde Pena <econdepe@proton.ch>
2023-11-14 19:52:06 +0100

    Avoid circular dependencies in Jest

--- packages/components/hooks/useIsElectronApp.ts
+++ packages/components/hooks/useIsElectronApp.ts
@@ -1,5 +1,6 @@
-import { FeatureFlag, useFlag } from '@proton/components/index';
 import { isElectronApp, isElectronOnMac as testIsElectronOnMac } from '@proton/shared/lib/helpers/desktop';
+
+import { FeatureFlag, useFlag } from '../containers/unleash';
 
 const useIsElectronApp = (flag: FeatureFlag) => {
     const isElectron = isElectronApp();
