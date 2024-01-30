783c6fd9dd3ed340c87012152edf42ec9adcc770
MargeBot <321-margebot@users.noreply.gitlab.protontech.ch>
2024-01-30 15:27:44 +0000

    Merge branch 'safari-16-font-fix' into 'main'
    Safari 16 font fix
    
    See merge request web/clients!8851
    
    Changelog:

--- applications/pass/src/app/index.tsx
+++ applications/pass/src/app/index.tsx
@@ -1,8 +1,11 @@
 import ReactDOM from 'react-dom';
 
 import '@proton/polyfill';
+import { initSafariFontFixClassnames } from '@proton/shared/lib/helpers/initSafariFontFixClassnames';
 
 import { App } from './App';
 import './style';
+
+initSafariFontFixClassnames();
 
 ReactDOM.render(<App />, document.querySelector('.app-root'));
